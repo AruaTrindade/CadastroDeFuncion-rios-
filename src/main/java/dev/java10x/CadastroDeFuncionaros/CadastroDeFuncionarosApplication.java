33ca1a93dda52ba9e5c27af800ff316f6d463e1b
Arua Trindade <seuemail@gmail.com>
2026-08-12 15:33:30 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/CadastroDeFuncionarosApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeFuncionaros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeFuncionarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeFuncionarosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeFuncionaros/CadastroDeFuncionarosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeFuncionaros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionarosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
