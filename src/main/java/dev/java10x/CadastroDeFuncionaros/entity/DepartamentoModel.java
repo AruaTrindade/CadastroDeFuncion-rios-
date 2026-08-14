a91d2a25e35c2bba3d285a6408dc787706509af1
Arua Trindade <seuemail@gmail.com>
2026-08-14 09:04:21 -0300

    feat: Cria lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeFuncionaros/entity/DepartamentoModel.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/entity/DepartamentoModel.java
@@ -1,11 +1,17 @@
 package dev.java10x.CadastroDeFuncionaros.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_departamento")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartamentoModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeFuncionaros/entity/FuncionarioModel.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/entity/FuncionarioModel.java
@@ -1,12 +1,18 @@
 package dev.java10x.CadastroDeFuncionaros.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //Entity transforma uma classe em uma entidade do banco de dados
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FuncionarioModel {
 
     @Id
@@ -21,16 +27,6 @@ public class FuncionarioModel {
     @ManyToOne
     @JoinColumn(name = "departamentos_id") //Chave estrangeira / foreing key
     private DepartamentoModel departamentos;
-
-    public FuncionarioModel(){
-
-    }
-
-    public FuncionarioModel(String name, String email, Integer idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
 
 
 
