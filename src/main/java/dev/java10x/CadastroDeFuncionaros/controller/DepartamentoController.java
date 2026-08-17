7a7632fd031dfca25ed1e207c7a6e957b5ffa8ef
Arua Trindade <seuemail@gmail.com>
2026-08-17 14:21:18 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeFuncionaros/controller/DepartamentoController.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/controller/DepartamentoController.java
@@ -1,9 +1,7 @@
 package dev.java10x.CadastroDeFuncionaros.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departamento")
@@ -12,5 +10,38 @@ public class DepartamentoController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
+    }
+    //CRUD:
+
+
+    // Adcionar Departamento - CREATE
+    @PostMapping("/criar")
+    public String criarDepartamento(){
+        return "Departamento criado";
+    }
+
+    // Procurar todos Departamentos - READ
+    @GetMapping("/todos")
+    public String mostrarTodosOsDepartamentos(){
+        return "Mostrar todos os departamentos";
+    }
+
+    // Procurar por id - READ
+    @GetMapping("/todosID")
+    public String mostrarTodosOsDepartamentosPorId(){
+        return "Mostrar Departamentos por id";
+    }
+
+
+    // Alterar dados dos Departamentos - UPDATE
+    @PutMapping("/alterarID")
+    public String alterarDepartamentoPorId(){
+        return "Departamento alterado";
+    }
+
+    // Deletar Departamento - DELETE
+    @DeleteMapping("/deletarID")
+    public String deletarDepartamentoPorId(){
+        return "Departamento deletado por id";
     }
 }
--- src/main/java/dev/java10x/CadastroDeFuncionaros/controller/FuncionarioController.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/controller/FuncionarioController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeFuncionaros.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/funcionario")
@@ -12,4 +10,38 @@ public class FuncionarioController {
     public String boasVindas() {
         return "Essa é minha primeira mensagem";
     }
+    //CRUD:
+
+
+    // Adcionar funcionario - CREATE
+    @PostMapping("/criar")
+    public String criarFuncionario(){
+        return "Funcionario criado";
+    }
+
+    // Procurar todos funcionarios - READ
+    @GetMapping("/todos")
+    public String mostrarTodosOsFuncionarios(){
+        return "Mostrar todos os funcionarios";
+    }
+
+    // Procurar por id - READ
+    @GetMapping("/todosID")
+    public String mostrarTodosOsFuncionariosPorId(){
+        return "Mostrar funcionario por id";
+    }
+
+
+    // Alterar dados dos funcionarios - UPDATE
+    @PutMapping("/alterarID")
+    public String alterarFuncionarioPorId(){
+        return "Funcionario alterado";
+    }
+
+    // Deletar ninja - DELETE
+    @DeleteMapping("/deletarID")
+    public String deletarFuncionarioPorId(){
+        return "Ninja deletado por id";
+    }
+
 }
