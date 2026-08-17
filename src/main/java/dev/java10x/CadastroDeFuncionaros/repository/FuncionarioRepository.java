0bbee9934bbc31c1c253985b39101a2b0ba4845b
Arua Trindade <seuemail@gmail.com>
2026-08-17 09:12:09 -0300

    build: Conecta o banco de dados H2 a minha aplicacao Spring

--- src/main/java/dev/java10x/CadastroDeFuncionaros/repository/FuncionarioRepository.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/repository/FuncionarioRepository.java
@@ -1,6 +1,7 @@
 package dev.java10x.CadastroDeFuncionaros.repository;
 
+import dev.java10x.CadastroDeFuncionaros.entity.FuncionarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FuncionarioRepository extends JpaRepository {
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
 }
