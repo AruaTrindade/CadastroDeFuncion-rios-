0e36fdd25b4111c2c53093c56de26d361e363b45
Arua Trindade <seuemail@gmail.com>
2026-08-19 08:19:50 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no mapper os caminho entidade x dto

--- src/main/java/dev/java10x/CadastroDeFuncionaros/dto/FuncionarioDTO.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/dto/FuncionarioDTO.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeFuncionaros.dto;
 
+import dev.java10x.CadastroDeFuncionaros.entity.DepartamentoModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String imgUrl;
+
+    private Integer idade;
+
+    private DepartamentoModel departamentos;
+
+
+
+
 }
--- src/main/java/dev/java10x/CadastroDeFuncionaros/mapper/DepartamentoMapper.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/mapper/DepartamentoMapper.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeFuncionaros.mapper;
 
+import dev.java10x.CadastroDeFuncionaros.dto.DepartamentoDTO;
+import dev.java10x.CadastroDeFuncionaros.entity.DepartamentoModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DepartamentoMapper {
+
+    public DepartamentoModel map(DepartamentoDTO departamentoDTO){
+        DepartamentoModel departamentoModel =  new DepartamentoModel();
+
+        departamentoModel.setId(departamentoDTO.getId());
+        departamentoModel.setNomeDoDepartamento(departamentoDTO.getNomeDoDepartamento());
+        departamentoModel.setResponsavel(departamentoDTO.getResponsavel());
+        departamentoModel.setFuncionarios(departamentoDTO.getFuncionarios());
+
+        return  departamentoModel;
+    }
+
+    public DepartamentoDTO map(DepartamentoModel departamentoModel){
+        DepartamentoDTO departamentoDTO = new DepartamentoDTO();
+
+        departamentoDTO.setId(departamentoModel.getId());
+        departamentoDTO.setNomeDoDepartamento(departamentoModel.getNomeDoDepartamento());
+        departamentoDTO.setResponsavel(departamentoModel.getResponsavel());
+        departamentoDTO.setFuncionarios(departamentoModel.getFuncionarios());
+
+        return departamentoDTO;
+    }
+
 }
