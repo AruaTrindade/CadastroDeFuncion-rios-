627277644db6c31c2d2720c9331bd8701e0d90ec
Arua Trindade <seuemail@gmail.com>
2026-08-19 10:36:45 -0300

    refactor: Refatora o controller para incluir o ResponseEntity nas rotas para customizar as requisicoes do cliente

--- src/main/java/dev/java10x/CadastroDeFuncionaros/controller/DepartamentoController.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/controller/DepartamentoController.java
@@ -2,9 +2,12 @@ package dev.java10x.CadastroDeFuncionaros.controller;
 
 
 import dev.java10x.CadastroDeFuncionaros.dto.DepartamentoDTO;
+import dev.java10x.CadastroDeFuncionaros.dto.FuncionarioDTO;
 import dev.java10x.CadastroDeFuncionaros.entity.DepartamentoModel;
 import dev.java10x.CadastroDeFuncionaros.entity.FuncionarioModel;
 import dev.java10x.CadastroDeFuncionaros.service.DepartamentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,8 +32,10 @@ public class DepartamentoController {
 
     // Adcionar Departamento - CREATE
     @PostMapping("/criar")
-    public DepartamentoDTO criarDepartamento(@RequestBody DepartamentoDTO departamentoDTO){
-        return departamentoService.criarDepartamento(departamentoDTO);
+    public ResponseEntity<String> criarDepartamento(@RequestBody DepartamentoDTO departamento){
+        DepartamentoDTO departamentoDTO = departamentoService.criarDepartamento(departamento);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Departamento criado com sucesso: " + departamentoDTO.getNomeDoDepartamento() + ", ID: " + departamentoDTO.getId());
     }
 
     // Procurar todos Departamentos - READ
@@ -41,21 +46,41 @@ public class DepartamentoController {
 
     // Procurar por id - READ
     @GetMapping("/listar/{id}")
-    public DepartamentoDTO listarDepartamentosPorId(@PathVariable Long id){
-        return departamentoService.listarDepartamentosPorId(id);
+    public ResponseEntity<?> listarDepartamentosPorId(@PathVariable Long id){
+        DepartamentoDTO departamentos = departamentoService.listarDepartamentosPorId(id);
+        if(departamentos != null){
+            return ResponseEntity.ok(departamentos);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("departamento com esse id não existe nos nossos registros");
+        }
+
     }
 
 
     // Alterar dados dos Departamentos - UPDATE
     @PutMapping("/alterar/{id}")
-    public DepartamentoDTO alterarDepartamentoPorId(@PathVariable Long id, @RequestBody DepartamentoDTO departamentoDTO){
-        return departamentoService.atualizarDepartamento(id, departamentoDTO);
+    public ResponseEntity<?> alterarDepartamentoPorId(@PathVariable Long id, @RequestBody DepartamentoDTO departamentoDTO){
+        DepartamentoDTO departamentos = departamentoService.atualizarDepartamento(id, departamentoDTO);
+        if(departamentos != null){
+            return ResponseEntity.ok(departamentos);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Departamento com esse id não foi encontrado");
+        }
     }
 
     // Deletar Departamento - DELETE
     @DeleteMapping("/deletar/{id}")
-    public void deletarDepartamentoPorId(@PathVariable Long id){
-        departamentoService.deletarDepartamentoPorId(id);
+    public ResponseEntity<String> deletarDepartamentoPorId(@PathVariable Long id){
+        if(departamentoService.listarDepartamentosPorId(id) != null){
+            departamentoService.deletarDepartamentoPorId(id);
+            return ResponseEntity.ok("Departamento com ID:" + id+  " deletado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O departamento com id: " + id + " nao foi encontrado");
+        }
+
     }
 
 }
--- src/main/java/dev/java10x/CadastroDeFuncionaros/controller/FuncionarioController.java
+++ src/main/java/dev/java10x/CadastroDeFuncionaros/controller/FuncionarioController.java
@@ -3,6 +3,9 @@ package dev.java10x.CadastroDeFuncionaros.controller;
 import dev.java10x.CadastroDeFuncionaros.dto.FuncionarioDTO;
 import dev.java10x.CadastroDeFuncionaros.entity.FuncionarioModel;
 import dev.java10x.CadastroDeFuncionaros.service.FuncionarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,33 +29,56 @@ public class FuncionarioController {
 
     // Adcionar funcionario - CREATE
     @PostMapping("/criar")
-    public FuncionarioDTO criarFuncionario(@RequestBody FuncionarioDTO funcionarioDTO){
-        return funcionarioService.criarFuncionario(funcionarioDTO);
+    public ResponseEntity<String> criarFuncionario(@RequestBody FuncionarioDTO funcionario){
+        FuncionarioDTO funcionarioDTO = funcionarioService.criarFuncionario(funcionario);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Funcionario criado com sucesso: " + funcionarioDTO.getName() + ", ID: " + funcionarioDTO.getId());
     }
 
     // Procurar todos funcionarios - READ
     @GetMapping("/listar")
-    public List<FuncionarioDTO> listarFuncionarios(){
-        return funcionarioService.listarFuncionarios();
+    public ResponseEntity<List<FuncionarioDTO>> listarFuncionarios(){
+        List<FuncionarioDTO> funcionarios = funcionarioService.listarFuncionarios();
+        return ResponseEntity.ok(funcionarios);
     }
 
     // Procurar por id - READ
     @GetMapping("/listar/{id}")
-    public FuncionarioDTO listarFuncionariosPorId(@PathVariable Long id){
-        return funcionarioService.listarFuncionariosPorId(id);
+    public ResponseEntity<?> listarFuncionariosPorId(@PathVariable Long id){
+        FuncionarioDTO funcionarios = funcionarioService.listarFuncionariosPorId(id);
+        if(funcionarios != null){
+            return ResponseEntity.ok(funcionarios);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Funcionario com esse id não existe nos nossos registros");
+        }
+
     }
 
 
     // Alterar dados dos funcionarios - UPDATE
     @PutMapping("/alterar/{id}")
-    public FuncionarioDTO alterarFuncionarioPorId(@PathVariable Long id, @RequestBody FuncionarioDTO funcionarioDTO){
-        return funcionarioService.atualiarFuncionario(id, funcionarioDTO);
+    public ResponseEntity<?> alterarFuncionarioPorId(@PathVariable Long id, @RequestBody FuncionarioDTO funcionarioDTO){
+        FuncionarioDTO funcionarios = funcionarioService.atualiarFuncionario(id, funcionarioDTO);
+        if(funcionarios != null){
+            return ResponseEntity.ok(funcionarios);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Funcionario com esse id não foi encontrado");
+        }
     }
 
     // Deletar ninja - DELETE
     @DeleteMapping("/deletar/{ID}")
-    public void deletarFuncionarioPorId(@PathVariable Long id){
-        funcionarioService.deletarFuncionarioPorId(id);
+    public ResponseEntity<String> deletarFuncionarioPorId(@PathVariable Long id){
+        if(funcionarioService.listarFuncionariosPorId(id) != null){
+            funcionarioService.deletarFuncionarioPorId(id);
+            return ResponseEntity.ok("Funcionario com ID:" + id+  " deletado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O funcionario com id: " + id + " nao foi encontrado");
+        }
+
     }
 
 }
